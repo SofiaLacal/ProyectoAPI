dea49210d86a37397cf8a88f870e9d030eb066d7
Sofia Lacal <sofialacrodriguez@outlook.com>
2025-06-04 14:13:51 +0200

    Añadidos
    División en 3 columnas + fetch

--- javascript.js
+++ javascript.js
@@ -3,15 +3,21 @@ window.addEventListener("DOMContentLoaded", function(){
     let inputInfo = this.document.getElementById("infoIntro");
     let imagen = this.document.getElementById("imagenAPI");
     let divInfo = this.document.getElementById("infoAPI");
-    let item = this.document.getElementById("itemAPI");
+    let divGenero = this.document.getElementById("infoGenero");
+    let divEstado = this.document.getElementById("infoEstado");
+    
+
 
     btnPeticion.addEventListener("click", pedirInfo);
     
     function pedirInfo(){
 
-        let nombrePersonaje = inputInfo.value.toUpperCase();
+        let idPersonaje = inputInfo.value;
+        let genero = inputInfo.value;
+        let estado = inputInfo.value;
 
-        fetch("URL API" + nombrePersonaje)
+        // Búsqueda por ID
+        fetch("https://rickandmortyapi.com/api/character/" + idPersonaje)
 
             .then(res => {
                 if(!res.ok) {
@@ -29,17 +35,90 @@ window.addEventListener("DOMContentLoaded", function(){
                 imagen.src="";
                 divInfo.innerHTML = `<p id="rojo">${error.message}</p>`;
             })
+
+        //Búsqueda por género
+         fetch("https://rickandmortyapi.com/api/character/" + genero)
+
+            .then(res => {
+                if(!res.ok) {
+                    throw new Error("Personaje no encontrado") 
+                }
+                return res.json();
+            })
+
+            .then(data => {
+                console.log(data)
+                mostrarEstado(data)
+            })
+
+            .catch(error => {
+                imagen.src="";
+                divInfo.innerHTML = `<p id="rojo">${error.message}</p>`;
+            })
+
+        //Búsqueda por género
+         fetch("https://rickandmortyapi.com/api/character/" + estado)
+
+            .then(res => {
+                if(!res.ok) {
+                    throw new Error("Personaje no encontrado") 
+                }
+                return res.json();
+            })
+
+            .then(data => {
+                console.log(data)
+                mostrarGenero(data)
+            })
+
+            .catch(error => {
+                imagen.src="";
+                divInfo.innerHTML = `<p id="rojo">${error.message}</p>`;
+            })
+
+
     }
 
     function mostrarDatos(data){
-        let urlImagen = /*PONER AQUI EL VALOR DE LA IMAGEN DE LA API;*/
+        let urlImagen = data.image
         imagen.src = urlImagen;
 
-        let nombre = /*PONER AQUI EL VALOR DEL NOMBRE DE LA API;*/
-        /*PONER AQUI LAS CARACTERÍSTICAS QUE QUERAMOS SACAR DE LA API*/ 
+        let nombre = data.name 
+        let especie = data.species
+        let estatus = data.status
+        let genero = data.gender
+        let origen = data.origin.name
 
-        /*PONER LA CADENA DE INFORMACIÓN QUE QUERAMOS SACAR*/
-        divInfo.innerHTML =  `<p><strong>${nombre}</strong></p><p><strong>Altura: </strong>${altura}</p>
-                            <p><strong>Peso: </strong>${peso}</p><p><strong>Item: </strong>${nomItem}</p>`
+        divInfo.innerHTML =  `<p><strong>${nombre}</strong></p>
+        <p><strong>Género: </strong>${genero}</p>
+        <p><strong>Especie: </strong>${especie}</p>
+        <p><strong>Estado: </strong>${estatus}</p>
+        <p><strong>Origen: </strong>${origen}</p>`
+    }
+
+    function mostrarEstado(data){
+        let urlImagen = data.image
+        imagen.src = urlImagen;
+
+        let nombre = data.name 
+        let estatus = data.status
+
+        divEstado.innerHTML =  `<p><strong>${nombre}</strong></p>
+        <p><strong>Género: </strong>${genero}</p>
+        <p><strong>Especie: </strong>${especie}</p>
+        <p><strong>Estado: </strong>${estatus}</p>
+        <p><strong>Origen: </strong>${origen}</p>`
+
+    }
+
+    function mostrarGenero(data){
+        let urlImagen = data.image
+        imagen.src = urlImagen;
+
+        let nombre = data.name 
+        let genero = data.gender
+
+        divGenero.innerHTML =  `<p><strong>${nombre}</strong></p>
+        <p><strong>Género: </strong>${genero}</p>`
     }
 })
